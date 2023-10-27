5ba4b2bb6335ebfe1f2f95056eea2ad53daaa2f6
zc <zc-yun@outlook.com>
2023-10-27 23:32:06 +0800

    1

--- plugins/zp113_页面访问曲线.js
+++ plugins/zp113_页面访问曲线.js
@@ -2,51 +2,49 @@ const ARR = [1, 2, 3]
 
 function init({ container, exc, props }) {
     ARR.forEach(a => container.appendChild(document.createElement("div")))
-    exc('load("https://z.zccdn.cn/vendor/highcharts/highcharts_11.1.js")', null, () => { // https://code.hcharts.cn/
-        Highcharts.setOptions({ global: { useUTC: false }, accessibility: { enabled: false } })
+    exc('load("https://cdn.bootcdn.net/ajax/libs/echarts/5.4.3/echarts.min.js")', null, () => { // https://code.hcharts.cn/
         exc(`$traffic.page3("${props.page || ""}")`, null, R => {
             if (!R) return
             let x
             let data = []
             Object.keys(R.date).forEach(K => {
-                x = K.substr(0, 4) + "/" + K.substr(4, 2) + "/" + K.substr(-2) + " "
-                Object.keys(R.date[K]).forEach(k => {
-                    data.push([new Date(x + k + ":00").getTime(), R.date[K][k]])
-                })
+                x = K.substr(0, 4) + "-" + K.substr(4, 2) + "-" + K.substr(-2) + " "
+                Object.keys(R.date[K]).forEach(k => data.push([x + k + ":00", R.date[K][k]]))
             })
             data.sort((a, b) => b[0] - a[0])
-            let chart = JSON.parse(JSON.stringify(chartOption))
-            chart.title = { text: "最近3天浏览量 (" + data.reduce((acc, x) => acc + x[1], 0).toLocaleString("en-US") + ")" }
-            chart.series = [{ type: "area", name: "浏览量", data }]
-            Highcharts.chart(container.firstChild, chart)
 
+            const opt = { title: { left: "center", text: "访问量" }, xAxis: { type: "category" }, yAxis: { type: "value" }, tooltip: { trigger: "axis" }, series: [{ type: "line", smooth: true }] }
+            opt.title.text = "最近3天浏览量 (" + fm(data.reduce((acc, x) => acc + x[1], 0)) + ")"
+            opt.xAxis.data = data.map(a => a[0])
+            opt.series[0].data = data.map(a => a[1])
+            echarts.init(container.firstChild).setOption(opt)
+            //
             data = []
             Object.keys(R.month).forEach(K => {
-                x = K.substr(0, 4) + "/" + K.substr(-2) + "/"
-                Object.keys(R.month[K]).forEach(k => {
-                    data.push([new Date(x + k + " 01:00").getTime(), R.month[K][k]])
-                })
+                x = K.substr(0, 4) + "-" + K.substr(-2) + "-"
+                Object.keys(R.month[K]).forEach(k => data.push([x + k, R.month[K][k]]))
             })
             data.sort((a, b) => b[0] - a[0])
-            chart = JSON.parse(JSON.stringify(chartOption))
-            chart.title = { text: "最近3个月浏览量 (" + data.reduce((acc, x) => acc + x[1], 0).toLocaleString("en-US") + ")" }
-            chart.series = [{ type: "area", name: "浏览量", data }]
-            log(chart)
-            Highcharts.chart(container.children[1], chart)
-
+            opt.title.text = "最近3个月浏览量 (" + fm(data.reduce((acc, x) => acc + x[1], 0)) + ")"
+            opt.xAxis.data = data.map(a => a[0])
+            opt.series[0].data = data.map(a => a[1])
+            echarts.init(container.children[1]).setOption(opt)
+            //
             data = []
             Object.keys(R.year).forEach(K => {
-                Object.keys(R.year[K]).forEach(k => {
-                    data.push([new Date(K + "/" + k + "/01").getTime(), R.year[K][k]])
-                })
+                Object.keys(R.year[K]).forEach(k => data.push([K + "-" + k, R.year[K][k]]))
             })
             data.sort((a, b) => b[0] - a[0])
-            chart = JSON.parse(JSON.stringify(chartOption))
-            chart.title = { text: "各月浏览量 (" + data.reduce((acc, x) => acc + x[1], 0).toLocaleString("en-US") + ")" }
-            chart.series = [{ type: "area", name: "浏览量", data }]
-            Highcharts.chart(container.lastChild, chart)
+            opt.title.text = "各月浏览量 (" + fm(data.reduce((acc, x) => acc + x[1], 0)) + ")"
+            opt.xAxis.data = data.map(a => a[0])
+            opt.series[0].data = data.map(a => a[1])
+            echarts.init(container.lastChild).setOption(opt)
         })
     })
+}
+
+function fm(n) {
+    return n.toLocaleString("en-US")
 }
 
 $plugin({
@@ -59,21 +57,3 @@ $plugin({
     }],
     init
 })
-
-const dateTime = { "minute": "%H:%M", "hour": "%H:%M", "day": "%m-%d" }
-const chartOption = {
-    xAxis: { type: "datetime", dateTimeLabelFormats: dateTime },
-    yAxis: { title: "" },
-    tooltip: { dateTimeLabelFormats: dateTime },
-    plotOptions: {
-        series: {
-            fillColor: {
-                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-                stops: [
-                    [0, "#7cb5ec"],
-                    [1, "rgba(124,181,236,0)"]
-                ]
-            }
-        }
-    }
-}
